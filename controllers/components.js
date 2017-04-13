4c7bc6ee371ac311a10b9fc1bcecf1e7e1186c33
frank <franklin-2014@hotmail.com>
2017-04-13 17:41:56 -0500

    "Auth8"

--- controllers/components.js
+++ controllers/components.js
@@ -4,8 +4,8 @@
 let SUCCESS = 1;
 let ERROR = -1;
 
-let CORRECT_STATUS_COMPONENT = "Correcto";
-let ERROR_STATUS_COMPONENT = "Error";
+//let CORRECT_STATUS_COMPONENT = "Correcto";
+//let ERROR_STATUS_COMPONENT = "Error";
 
 let URL_BASE = "https://plataformamec.com/api/";
 let URL_ACCELEROMETER = URL_BASE + "accelerometer";
@@ -16,268 +16,314 @@ let URL_CPU = URL_BASE + "cpu";
 let URL_BATTERY = URL_BASE + "battery";
 let URL_GPS = URL_BASE + "gps";
 
-var Client = require('node-rest-client').Client;
-var client = new Client();
+const DIR_COMPONENTS = "/home/debian/Sensor-IOT/SensorIoT/componentsFiles";
+const DIR_ACCELETOMETER = DIR_COMPONENTS + "accelerometer.json";
+const DIR_ADC = DIR_COMPONENTS + "adc.json";
+const DIR_RTC = DIR_COMPONENTS + "rtc.json"; 
+const DIR_WIFI = DIR_COMPONENTS + "wifi.json";
+const DIR_CPU = DIR_COMPONENTS  + "cpu.json";
+const DIR_BATTERY = DIR_COMPONENTS  + "battery.json";
+const DIR_GPS = DIR_COMPONENTS + "gps.json";
 
-var auth = require('./auth.js');
-//var exit = require('./exit.js');
+let Client = require('node-rest-client').Client;
+let client = new Client();
+
+let auth = require('./auth.js');
+//let exit = require('./exit.js');
 let fs = require('fs');
-
-var authToken = "";
-
-/*exports.authInit = auth.doAuth(function(code, token){
-    if(code == ERROR){
-        //exit.preExitFunc();
-    }else{
-        authToken = token;
-        console.log(token);
-
-        //var config = require('./componentsFiles/accelerometer.json');
-        //console.log(config);
-        acelerometerData(token);
-        adcData(token);
-        rtcData(token);
-        cpuData(token);
-        batteryData(token);
-        gpsData(token);
-        this.acelerometerData(token);
-    }
-
-});*/
 
 exports.acelerometerData = function acelerometerData (token) {
 
-    console.log("acelerometerData");
+    return new Promise(
+        function(fullfil) {
 
-    fs.readFile('componentsFiles/accelerometer.json', 'utf-8', (err, json) => {
-        if(err) {
-            console.log('error: ', err);
-        }
-        else {
+            console.log("acelerometerData");
 
-            var jsonObj = JSON.parse(json);
-
-            var args = {
-                data: jsonObj,
-                headers: {"Content-Type": "application/json",
-                    "Authorization":token,}
-            };
-
-            console.log(args);
-
-            client.post(URL_ACCELEROMETER, args, function (data, response) {
-                console.log("acelerometerData");
-                var jsonObj = data;
-                console.log(jsonObj);
-                if (jsonObj.code == "001") {
-                    code = SUCCESS ;
-                    //callback(code,token);
+            fs.readFile(DIR_ACCELETOMETER, 'utf-8', (err, json) => {
+                if (err) {
+                    console.log('error: ', err);
+                    fullfil({code: ERROR});
                 }
                 else {
-                    code = ERROR ;
-                    //exit.preExitFunc();
-                    //callback(code,token);
+
+                    let jsonObj = JSON.parse(json);
+
+                    let args = {
+                        data: jsonObj,
+                        headers: {
+                            "Content-Type": "application/json",
+                            "Authorization": token,
+                        }
+                    };
+
+                    console.log(args);
+
+                    client.post(URL_ACCELEROMETER, args, function (data, response) {
+                        console.log("acelerometerData");
+                        let jsonObj = data;
+                        console.log(jsonObj);
+                        if (jsonObj.code === "001") {
+                            fullfil({code: SUCCESS});
+                        }
+                        else {
+                            fullfil({code: ERROR});
+                        }
+                    });
+
                 }
             });
-
-        }
-    });
-}
+        });
+};
 
 exports.adcData = function adcData (token) {
-    console.log("adcData");
 
-    fs.readFile('componentsFiles/adc.json', 'utf-8', (err, json) => {
-        if(err) {
-            console.log('error: ', err);
-        }
-        else {
+    return new Promise(
+        function(fullfil) {
 
-            var jsonObj = JSON.parse(json);
-            var args = {
-                data: jsonObj,
-                headers: {"Content-Type": "application/json",
-                    "Authorization":token,}
-            };
+            console.log("adcData");
 
-            console.log(args);
-
-            client.post(URL_ADC, args, function (data, response) {
-                console.log("adcData");
-                var jsonObj = data;
-                console.log(jsonObj);
-                if (jsonObj.code == "001") {
-                    code = SUCCESS ;
-                    //callback(code,token);
+            fs.readFile(DIR_ADC, 'utf-8', (err, json) => {
+                if (err) {
+                    console.log('error: ', err);
+                    fullfil({code: ERROR});
                 }
                 else {
-                    code = ERROR ;
-                    //exit.preExitFunc();
-                    //callback(code,token);
+
+                    let jsonObj = JSON.parse(json);
+                    let args = {
+                        data: jsonObj,
+                        headers: {
+                            "Content-Type": "application/json",
+                            "Authorization": token,
+                        }
+                    };
+
+                    console.log(args);
+
+                    client.post(URL_ADC, args, function (data, response) {
+                        console.log("adcData");
+                        let jsonObj = data;
+                        console.log(jsonObj);
+                        if (jsonObj.code === "001") {
+                            fullfil({code: SUCCESS});
+                        }
+                        else {
+                            fullfil({code: ERROR});
+                        }
+                    });
                 }
             });
-        }
-    });
-
-}
+        });
+};
 
 exports.rtcData = function rtcData(token) {
-    console.log("rtcData");
 
-    fs.readFile('componentsFiles/rtc.json', 'utf-8', (err, json) => {
-        if(err) {
-            console.log('error: ', err);
-        }
-        else {
+    return new Promise(
+        function(fullfil) {
 
-            var jsonObj = JSON.parse(json);
+            console.log("rtcData");
 
-            var args = {
-                data: jsonObj,
-                headers: {"Content-Type": "application/json",
-                    "Authorization":token,}
-            };
-
-            console.log(args);
-
-            client.post(URL_RTC, args, function (data, response) {
-                console.log("rtcData");
-                var jsonObj = data;
-                console.log(jsonObj);
-                if (jsonObj.code == "001") {
-                    code = SUCCESS ;
-                    //callback(code,token);
+            fs.readFile(DIR_RTC, 'utf-8', (err, json) => {
+                if(err) {
+                    console.log('error: ', err);
+                    fullfil({code: ERROR});
                 }
                 else {
-                    code = ERROR ;
-                    //exit.preExitFunc();
-                    //callback(code,token);
+
+                    let jsonObj = JSON.parse(json);
+
+                    let args = {
+                        data: jsonObj,
+                        headers: {"Content-Type": "application/json",
+                            "Authorization":token,}
+                    };
+
+                    console.log(args);
+
+                    client.post(URL_RTC, args, function (data, response) {
+                        console.log("rtcData");
+                        let jsonObj = data;
+                        console.log(jsonObj);
+                        if (jsonObj.code === "001") {
+                            fullfil({code: SUCCESS});
+                        }
+                        else {
+                            fullfil({code: ERROR});
+                        }
+                    });
                 }
             });
-        }
-    });
 
-}
+        });
+};
 
 
 exports.cpuData = function cpuData (token) {
-    console.log("cpuData");
 
-    fs.readFile('componentsFiles/cpu.json', 'utf-8', (err, json) => {
-        if(err) {
-            console.log('error: ', err);
-        }
-        else {
+    return new Promise(
+        function(fullfil) {
+            console.log("cpuData");
 
-            var jsonObj = JSON.parse(json);
-
-            var args = {
-                data: jsonObj,
-                headers: {"Content-Type": "application/json",
-                    "Authorization":token,}
-            };
-
-            console.log(args);
-
-            client.post(URL_CPU, args, function (data, response) {
-                console.log("cpuData");
-                var jsonObj = data;
-                console.log(jsonObj);
-                if (jsonObj.code == "001") {
-                    code = SUCCESS ;
-                    //callback(code,token);
+            fs.readFile(DIR_CPU, 'utf-8', (err, json) => {
+                if(err) {
+                    console.log('error: ', err);
+                    fullfil({code: ERROR});
                 }
                 else {
-                    code = ERROR ;
-                    //exit.preExitFunc();
-                    //callback(code,token);
+
+                    let jsonObj = JSON.parse(json);
+
+                    let args = {
+                        data: jsonObj,
+                        headers: {"Content-Type": "application/json",
+                            "Authorization":token,}
+                    };
+
+                    console.log(args);
+
+                    client.post(URL_CPU, args, function (data, response) {
+                        console.log("cpuData");
+                        let jsonObj = data;
+                        console.log(jsonObj);
+                        if (jsonObj.code === "001") {
+                            fullfil({code: SUCCESS});
+                            //callback(code,token);
+                        }
+                        else {
+                            fullfil({code: ERROR});
+                            //exit.preExitFunc();
+                            //callback(code,token);
+                        }
+                    });
                 }
             });
-        }
-    });
+        });
 
-}
+};
 
 exports.batteryData = function batteryData (token) {
-    console.log("batteryData");
 
-    fs.readFile('componentsFiles/battery.json', 'utf-8', (err, json) => {
-        if(err) {
-            console.log('error: ', err);
-        }
-        else {
+    return new Promise(
+        function(fullfil) {
 
-            var jsonObj = JSON.parse(json);
+            console.log("batteryData");
 
-            var args = {
-                data: jsonObj,
-                headers: {"Content-Type": "application/json",
-                    "Authorization":token,}
-            };
-
-            console.log(args);
-
-            client.post(URL_BATTERY, args, function (data, response) {
-                console.log("batteryData");
-                var jsonObj = data;
-                console.log(jsonObj);
-                if (jsonObj.code == "001") {
-                    code = SUCCESS ;
-                    //callback(code,token);
+            fs.readFile(DIR_BATTERY, 'utf-8', (err, json) => {
+                if(err) {
+                    console.log('error: ', err);
+                    fullfil({code: ERROR});
                 }
                 else {
-                    code = ERROR ;
-                    //exit.preExitFunc();
-                    //callback(code,token);
+
+                    let jsonObj = JSON.parse(json);
+
+                    let args = {
+                        data: jsonObj,
+                        headers: {"Content-Type": "application/json",
+                            "Authorization":token,}
+                    };
+
+                    console.log(args);
+
+                    client.post(URL_BATTERY, args, function (data, response) {
+                        console.log("batteryData");
+                        let jsonObj = data;
+                        console.log(jsonObj);
+                        if (jsonObj.code === "001") {
+                            fullfil({code: SUCCESS});
+                        }
+                        else {
+                            fullfil({code: ERROR});
+                        }
+                    });
                 }
             });
-        }
-    });
 
-}
+        });
+};
 
 exports.gpsData = function gpsData (token) {
-    console.log("gpsData");
 
-    fs.readFile('componentsFiles/gps.json', 'utf-8', (err, json) => {
-        if(err) {
-            console.log('error: ', err);
-        }
-        else {
+    return new Promise(
+        function(fullfil) {
 
-            var jsonObj = JSON.parse(json);
+            console.log("gpsData");
 
-            var args = {
-                data: jsonObj,
-                headers: {"Content-Type": "application/json",
-                    "Authorization":token,}
-            };
-
-            console.log(args);
-
-            client.post(URL_GPS, args, function (data, response) {
-                console.log("gpsData");
-                var jsonObj = data;
-                console.log(jsonObj);
-                if (jsonObj.code == "001") {
-                    code = SUCCESS ;
-                    //callback(code,token);
+            fs.readFile(DIR_GPS, 'utf-8', (err, json) => {
+                if(err) {
+                    fullfil({code: ERROR});
                 }
                 else {
-                    code = ERROR ;
-                    //exit.preExitFunc();
-                    //callback(code,token);
+
+                    let jsonObj = JSON.parse(json);
+
+                    let args = {
+                        data: jsonObj,
+                        headers: {"Content-Type": "application/json",
+                            "Authorization":token,}
+                    };
+
+                    console.log(args);
+
+                    client.post(URL_GPS, args, function (data, response) {
+                        console.log("gpsData");
+                        let jsonObj = data;
+                        console.log(jsonObj);
+                        if (jsonObj.code === "001") {
+                            fullfil({code: SUCCESS});
+                        }
+                        else {
+                            fullfil({code: ERROR});
+                        }
+                    });
+
                 }
             });
 
-        }
-    });
+        });
+};
 
-}
+exports.wifiData = function (token) {
 
-exports.wifiData = function () {
+    return new Promise(
+        function(fullfil) {
 
-}
+            console.log("wifiData");
+
+            fs.readFile(DIR_WIFI, 'utf-8', (err, json) => {
+                if(err) {
+                    console.log('error: ', err);
+                    fullfil({code: ERROR});
+                }
+                else {
+
+                    let jsonObj = JSON.parse(json);
+
+                    let args = {
+                        data: jsonObj,
+                        headers: {"Content-Type": "application/json",
+                            "Authorization":token,}
+                    };
+
+                    console.log(args);
+
+                    client.post(URL_GPS, args, function (data, response) {
+                        console.log("wifiData");
+                        let jsonObj = data;
+                        console.log(jsonObj);
+                        if (jsonObj.code === "001") {
+                            fullfil({code: SUCCESS});
+                        }
+                        else {
+                            fullfil({code: ERROR});
+                        }
+                    });
+
+                }
+            });
+
+        });
+};
 
 
 
--- routes/init.js
+++ routes/init.js
@@ -1,6 +1,8 @@
 /**
  * Created by Frank on 13/04/2017.
  */
+let SUCCESS = 1;
+let ERROR = -1;
 
 const express = require('express');
 const components = require('../controllers/components');
@@ -11,14 +13,55 @@ const router = express.Router();
 router.get('/', function(req, res, next) {
 
     auth.doAuth.then(function (data) {
-        if(data.code == -1){
+        if(data.code === ERROR){
             //exit.preExitFunc();
         }else{
             let authToken = data.token;
             console.log(authToken);
 
-            //var config = require('./componentsFiles/accelerometer.json');
-            //console.log(config);
+            components.acelerometerData(authToken).then(function (data) {
+                if(data.code === ERROR){
+                    res.status(401).send();
+                }else{
+                    components.adcData(authToken).then(function (data) {
+                        if(data.code === ERROR){
+                            res.status(401).send();
+                        }else{
+                            components.rtcData(authToken).then(function (data) {
+                                if(data.code === ERROR){
+                                    res.status(401).send();
+                                }else{
+                                    components.cpuData(authToken).then(function (data) {
+                                        if(data.code === ERROR){
+                                            res.status(401).send();
+                                        }else {
+                                            components.batteryData(authToken).then(function (data) {
+                                                if(data.code === ERROR){
+                                                    res.status(401).send();
+                                                }else{
+                                                    components.gpsData(authToken).then(function (data) {
+                                                        if(data.code === ERROR){
+                                                            res.status(401).send();
+                                                        } else{
+                                                            components.wifiData(authToken).then(function (data) {
+                                                                if(data.code === ERROR){
+                                                                    res.status(400).send();
+                                                                }else{
+                                                                    res.status(200).send();
+                                                                }
+                                                            })
+                                                        }
+                                                    })
+                                                }
+                                            });
+                                        }
+                                    });
+                                }
+                            });
+                        }
+                    });
+                }
+            });
             /*acelerometerData(token);
              adcData(token);
              rtcData(token);

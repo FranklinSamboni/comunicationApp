4542824b082c66e85d70a8c2fe39bd44e5dd9580
frank <franklin-2014@hotmail.com>
2017-04-13 18:41:24 -0500

    "Auth8"

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
+let cors = require('cors');
 var index = require('./routes/index');
 let init = require('./routes/init');
 
@@ -22,6 +22,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors());
 
 app.use('/', index);
 app.use('/init', init);

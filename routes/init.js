d63f531c15d8ddd4f36f35db463cd6dd038fb339
frank <franklin-2014@hotmail.com>
2017-04-13 18:22:11 -0500

    "Auth8"

--- routes/init.js
+++ routes/init.js
@@ -14,40 +14,42 @@ router.get('/', function(req, res, next) {
 
     auth.doAuth.then(function (data) {
         if(data.code === ERROR){
-            res.status(201).send({code:'002'});
+            return res.status(201).send({code:'002'});
         }else{
             let authToken = data.token;
             console.log(authToken);
 
             components.acelerometerData(authToken).then(function (data) {
                 if(data.code === ERROR){
-                    res.status(201).send({code:'002'});
+
+                    return res.status(201).send({code:'002'});
+
                 }else{
                     components.adcData(authToken).then(function (data) {
                         if(data.code === ERROR){
-                            res.status(201).send({code:'002'});
+                            return res.status(201).send({code:'002'});
                         }else{
                             components.rtcData(authToken).then(function (data) {
                                 if(data.code === ERROR){
-                                    res.status(201).send({code:'002'});
+                                    return res.status(201).send({code:'002'});
                                 }else{
                                     components.cpuData(authToken).then(function (data) {
                                         if(data.code === ERROR){
-                                            res.status(201).send({code:'002'});
+                                            return res.status(201).send({code:'002'});
                                         }else {
                                             components.batteryData(authToken).then(function (data) {
                                                 if(data.code === ERROR){
-                                                    res.status(201).send({code:'002'});
+                                                    return res.status(201).send({code:'002'});
                                                 }else{
                                                     components.gpsData(authToken).then(function (data) {
                                                         if(data.code === ERROR){
-                                                            res.status(201).send({code:'002'});
+                                                            return res.status(201).send({code:'002'});
                                                         } else{
                                                             components.wifiData(authToken).then(function (data) {
                                                                 if(data.code === ERROR){
-                                                                    res.status(201).send({code:'002'});
+                                                                    return res.status(201).send({code:'002'});
                                                                 }else{
-                                                                    res.status(201).send({code:'001'});
+                                                                    return res.status(201).send({code:'001'});
                                                                 }
                                                             })
                                                         }

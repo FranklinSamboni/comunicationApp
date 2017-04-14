38c6092a1b794220426f567aaa71b6af87991496
frank <franklin-2014@hotmail.com>
2017-04-14 14:30:50 -0500

    "Auth8"

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ let init = require('./routes/init');
 
 var app = express();
 
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -29,6 +30,7 @@ app.use('/init', init);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
+  console.log("404 and forward");
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
@@ -36,6 +38,7 @@ app.use(function(req, res, next) {
 
 // error handler
 app.use(function(err, req, res, next) {
+    console.log("Error handler");
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};

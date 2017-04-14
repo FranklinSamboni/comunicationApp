393694c558a2ceb318e5ee1227f1fa4b6d6d1895
frank <franklin-2014@hotmail.com>
2017-04-14 17:16:36 -0500

    "Auth8"

--- controllers/socketIoManager.js
+++ controllers/socketIoManager.js
@@ -4,12 +4,24 @@
 
 const URL_SOCKET = "https://socket.plataformamec.com/";
 
-let io = require('socket.io-client');
-
+const io = require('socket.io-client');
+const exec = require('child_process').exec;
 let socket = io.connect(URL_SOCKET);
 
 socket.on('connect', function () {
     console.log("socket connected");
+
+    //exec(`/home/debian/Sensor-IOT/SensorIoT/tests/testGps ${path_file}`,{maxBuffer: 1024 * 50000}, function (err, stdout, stderr) {
+    exec(`/home/debian/Sensor-IOT/SensorIoT/tests/testGps U`,{maxBuffer: 1024 * 50000}, function (err, stdout, stderr) {
+        console.log(err);
+        console.log(stdout);
+        console.log(stderr);
+
+        /*if(err) return fullfill({hcode: 202, code: "003", msg: "Error", data: err});
+
+        //console.log(stdout);
+        fullfill({hcode: 202, code: "001", msg: date_file, data: stdout})*/
+    })
 
     socket.emit('register', '{ "serial": "Q2SW4ER5T6" }', function(resp, data) {
         console.log('respuesta del servidor' + resp);

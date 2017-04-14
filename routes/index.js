706ac47dc27945764f0a145cfff43fe8dbf01f8b
frank <franklin-2014@hotmail.com>
2017-04-14 12:47:50 -0500

    "Auth8"

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,14 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
+
+console.log("Se inicio el index");
+
+router.all('/', function (req, res, next) {
+    console.log('Accessing the secret section ...');
+    //next(); // pass control to the next handler
+});
+
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
   res.status(200).send();

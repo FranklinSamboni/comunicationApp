e081726960d642560fc2fd1bca98edd1479aa508
frank <franklin-2014@hotmail.com>
2017-04-16 13:56:18 -0500

    "TestG"

--- controllers/socketServer.js
+++ controllers/socketServer.js
@@ -2,14 +2,29 @@
  * Created by Frank on 14/04/2017.
  */
 
-const UART = "UART";
+
+const TYPE_TEST =  "TEST";
+const TEST_UART = "TEST_UART";
+const TEST_PPS = "TEST_PPS";
+const TEST_ADC = "TEST_ADC";
+const TEST_RTC = "TEST_RTC";
+const TEST_SYNC = "TEST_SYNC";
+
+const TYPE_MAIN =  "MAIN";
+const PUT_LOCATION = "PUT_LOCATION_GPS";
+const PUT_RTC_DATE = "PUT_RTC_DATEHOUR";
+const PUT_SPS = "PUT_SPS";
+const ALERTS = "ALERTS_ERROR";
+const REAL_TIME = "REAL_TIME";
+
+const GPS = "GPS";
 const PPS = "PPS";
-const ADC = "ADC";
 const RTC = "RTC";
 const SYNC = "SYNC";
-
-const ALERTS = "ALERTS";
-const REAL_TIME = "REAL_TIME";
+const ADC = "ADC";
+const ACC = "ACC";
+const BAT = "BAT";
+const WIFI = "WIFI";
 
 const portSocket = 4001;
 
@@ -30,21 +45,28 @@ let socketServer = net.createServer( function (socket) {
         try {
             let json = JSON.parse(data);
             console.log(json);
-            switch (json.component) {
-                case UART:
+            switch (json.type) {
+                case TYPE_TEST:
                     doEmitTestResponse(json.msg, json.last);
                     break;
-                case PPS:
-                    doEmitTestResponse(json.msg, json.last);
-                    break;
-                case ADC:
-                    doEmitTestResponse(json.msg, json.last);
-                    break;
-                case RTC:
-                    doEmitTestResponse(json.msg, json.last);
-                    break;
-                case SYNC:
-                    doEmitTestResponse(json.msg, json.last);
+                case TYPE_MAIN:
+                    console.log(json.msg);
+                    switch (json.process){
+                        case PUT_LOCATION:
+                            break;
+                        case PUT_RTC_DATE:
+                            break;
+                        case PUT_SPS:
+                            break;
+                        case ALERTS:
+                            doEmitAlertError(json.msg,json.component);
+                            break;
+                        case REAL_TIME:
+                            break;
+                        default:
+                            break;
+                    }
+
                     break;
                 default:
                     break;
@@ -72,6 +94,17 @@ function doEmitTestResponse(msg, last) {
         let sendJson = `{"token": "${config.token}", "msg": "${msg}", "last" : ${last} }`;
         console.log("Antes del emit");
         socketClient.socket.emit('testResponse',sendJson, function(resp, data) {
+            console.log('respuesta del servidor' + resp);
+            console.log(resp.code);
+        });
+    }
+}
+
+function doEmitAlertError(msg, component) {
+    if (config.token !== ""){
+        let sendJson = `{"token": "${config.token}", "data": "${msg}", "component" : ${component} }`;
+        console.log("Antes del emit");
+        socketClient.socket.emit('requestError',sendJson, function(resp, data) {
             console.log('respuesta del servidor' + resp);
             console.log(resp.code);
         });

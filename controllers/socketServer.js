e5ff5d9c4a2567d90e0377c4079d990fcf97a4ab
frank <franklin-2014@hotmail.com>
2017-04-22 00:10:48 -0500

    "TestG"

--- controllers/socketServer.js
+++ controllers/socketServer.js
@@ -91,7 +91,7 @@ function doEmitTestResponse(msg, last) {
 
 function doEmitAlertError(msg, component) {
     if (config.token !== ""){
-        let sendJson = `{"token": "${config.SOCKET_TOKEN}", "data": "${msg}", "component" : ${component} }`;
+        let sendJson = `{"token": "${config.SOCKET_TOKEN}", "data": "${msg}", "component" : "${component}" }`;
         console.log("emit request error  " + msg + "   " + component);
         socketClient.socket.emit('requestError',sendJson, function(resp, data) {
             console.log('respuesta del servidor' + resp);
